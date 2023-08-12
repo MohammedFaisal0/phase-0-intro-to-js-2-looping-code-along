9ed1e88964ff94acbbd53f64b24ef9672e48193f
MohammedFaisal0 <alshamimohammed90@gmail.com>
2023-08-12 07:09:50 +0300

    add solution

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(names, message) {
+  let array = [];
+  for (let i = 0; i < names.length; i++) {
+    array[i] = `Thank you, ${names[i]}, for the wonderful ${message} gift!`
+  }
+  return array;
+}
+function countDown () {
+  for (let i=10; i>=0 ;i--){
+    console.log(i)
+  }
+}
